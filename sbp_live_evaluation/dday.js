b82a8b2d48d5bdb78799f801767a25f48d71d2ed
hyunwoo6321 <phw1508@naver.com>
2021-12-12 11:49:39 +0900

    마지막 날짜 변경

--- sbp_live_evaluation/dday.js
+++ sbp_live_evaluation/dday.js
@@ -1,7 +1,7 @@
 window.onload = async function () {
     while (true) {
         var today = new Date();
-        var anniversary = new Date(2021, 11, 8, 8, 50);
+        var anniversary = new Date(2022, 0, 4, 10, 00);
         var count = today.getTime() - anniversary.getTime();
         var day = Math.floor(count/(1000*60*60*24));
         var hours = Math.floor((count % (1000*60*60*24))/(1000*60*60));
@@ -9,13 +9,13 @@ window.onload = async function () {
         var seconds = Math.floor((count % (1000*60))/1000);
         var anni = document.getElementById("dday");
         if (isPositive(day) && isPositive(hours) && isPositive(minutes) && isPositive(seconds)) {
-            anni.innerHTML = "원서 접수 하는중!! 원하는 학교에 갈 수 있을거에요!"
+            anni.innerHTML = "중학교 졸업! 고등학교에서 즐거운 생활해요"
         } else {
             day = Math.abs(day) - 1
             hours = Math.abs(hours) - 1
             minutes = Math.abs(minutes) - 1
             seconds = Math.abs(seconds) - 1
-            anni.innerHTML = "일반고 원서 접수까지 " + day + "일 " + hours + "시간 " + minutes + "분 " + seconds + "초 ";
+            anni.innerHTML = "중학교 졸업까지 " + day + "일 " + hours + "시간 " + minutes + "분 " + seconds + "초 ";
         }
         await sleep(1000);
     }
